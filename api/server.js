44b8c74cc9b847c6c9bc9b7c85ba54d9b5766539
Rohan Hangal <rohanhangal@gmail.com>
2023-05-12 20:45:19 -0700

    foo is wired

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const server = express();
 
 server.use(express.json());
 
-server.use("api/posts", postRouter);
+server.use("/api/posts", postRouter);
 
 server.use("*", (req, res) => {
   res.status(404).json({
